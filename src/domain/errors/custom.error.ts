ad222e4e803acf6ef7adf96d7eade9c30a6e11ea
Jesús de la Fuente <jfuentelucas@gmail.com>
2024-11-09 18:08:25 +0100

    domain: CustomError

---
+++ src/domain/errors/custom.error.ts
@@ -0,0 +1,31 @@
+
+
+export class CustomError extends Error{
+    constructor (
+        public readonly statusCode: number,
+        public readonly message: string,
+       
+    ){
+        super(message);
+    }
+
+    // factory methods (to return the previously created instances):
+    static badRequest(message: string){
+        return new CustomError(400, message);
+    };
+    static unauthorized(message: string){
+        return new CustomError(401, message);
+    };
+    static forbidden(message: string){
+        return new CustomError(403, message);
+    };
+    static notFound(message: string){
+        return new CustomError(404, message);
+    };
+    static internalServer(message: string){
+        return new CustomError(500, message);
+    };
+
+
+
+}
